c414f636786989c93e3815be9e173a6fe0cc9d38
Thomas Uster <thomasuster@gmail.com>
2015-05-06 09:46:41 -0700

    iOS titles populate

--- extension/iap/ProductDetails.hx
+++ extension/iap/ProductDetails.hx
@@ -17,17 +17,18 @@ class ProductDetails
 		// Handle both Android and iOS Ids
 		productID = Reflect.hasField(dynObj, "productId")? Reflect.field(dynObj, "productId") : Reflect.field(dynObj, "productID");
 		type = cast Reflect.field(dynObj, "type");
-		localizedTitle = cast Reflect.field(dynObj, "title");
 		#if ios
 			localizedPrice = cast Reflect.field(dynObj, "localizedPrice");
 			priceAmountMicros = cast Reflect.field(dynObj, "priceAmountMicros");
 			priceCurrencyCode = cast Reflect.field(dynObj, "priceCurrencyCode");
 			localizedDescription = cast Reflect.field(dynObj, "localizedDescription");
+			localizedTitle = cast Reflect.field(dynObj, "localizedTitle");
 		#else
 			localizedPrice = cast Reflect.field(dynObj, "price");
 			priceAmountMicros = cast Reflect.field(dynObj, "price_amount_micros");
 			priceCurrencyCode = cast Reflect.field(dynObj, "price_currency_code");
 			localizedDescription = cast Reflect.field(dynObj, "description");
+			localizedTitle = cast Reflect.field(dynObj, "title");
 		#end
 		price = priceAmountMicros / 1000 / 1000;
 	}
